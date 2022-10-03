269659221d8e08bbb30fdc79e2cabd1c06e082cc
phongngotrong <ngotrongphong.dev@gmail.com>
2022-10-03 16:50:23 +0700

    toi uu header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,18 @@
 const menuToggle = document.querySelector(".header-toggle");
+const menuClose = document.querySelector(".header-close");
 const menuHeader = document.querySelector(".header-menu");
 const expandClass = "is-expand";
 
 menuToggle.addEventListener("click", function () {
   menuHeader.classList.add(expandClass);
+});
+
+menuClose.addEventListener("click", function () {
+  menuHeader.classList.remove(expandClass);
+});
+
+window.addEventListener("click", function (e) {
+  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
+    menuHeader.classList.remove(expandClass);
+  }
 });
