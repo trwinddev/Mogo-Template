62bd9cbfe61f739acb4e7f7067538344ee22df39
phongngotrong <ngotrongphong.dev@gmail.com>
2022-10-03 14:14:01 +0700

    first landing page

---
+++ public/scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";var menuToggle=document.querySelector(".header-toggle"),menuHeader=document.querySelector(".header-menu"),expandClass="is-expand";menuToggle.addEventListener("click",function(){menuHeader.classList.add(expandClass)});
